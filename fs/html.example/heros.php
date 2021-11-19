7a3a0e19eeca50c4580474687f30874a812b38ff
David Newcomb <david.newcomb@bigsoft.co.uk>
2021-11-19 02:48:52 +0000

    test files

---
+++ fs/html.example/heros.php
@@ -0,0 +1,32 @@
+<?php
+
+$user = "user_hero";
+$pass = "user_hero";
+$dbname = "heros";
+
+$dsn = "mysql:dbname=$dbname;host=localhost;port=3306;charset=utf8";
+$options = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query("SELECT * FROM hero");
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<html>";
+echo "<body>";
+echo '<table border="1">';
+foreach ($results as $r) {
+	echo "<tr>"
+		."<td>". $r["real_name"] ."</td>"
+		."<td>". $r["persona"] ."</td>"
+		."</tr>";
+}
+
+echo "</table>";
+echo "</body>";
+echo "</html>";
+
+?>
